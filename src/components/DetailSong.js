a8e2f3e32553fa96e3081d9b7efcc31d001137fa
Phong Nguyen <nglephong197@gmail.com>
2023-02-18 13:04:06 +0700

    Init spotify react app

---
+++ src/components/DetailSong.js
@@ -0,0 +1,19 @@
+import React, { useContext } from "react";
+import { Songs } from "../Context";
+
+export default function DetailSong() {
+    const {song} = useContext(Songs)
+    return(
+        <div className='col-span-1 text-center  flex flex-col justify-evenly'>
+            <h2 className='text-cyan-400 font-semibold font'>Now playing</h2>
+            <h2 className='text-gray-300 text-2xl'>{song.name}</h2>
+            <div className="w-[240px] mr-auto ml-auto">
+                <img className="rounded-xl" src={song.links.images[0].url} alt="avatar"></img>
+            </div>
+            <div className="flex justify-center items-center mt-4">
+                <img className="w-[70px] rounded-full" src={song.links.images[1].url} alt="avatar"></img>
+                <span className="text-xl text-white ml-5">{song.author}</span>
+            </div>
+        </div>
+    )
+}
---
+++ src/components/ListSongs.js
@@ -0,0 +1,44 @@
+import React, { useContext, useEffect, useState } from "react";
+import { Songs } from "../Context";
+
+export default function ListSongs() {
+    const {DataSongs, handleSetSong, song} = useContext(Songs)
+    const [idSong, setIdSong] = useState(0)
+    const handlePlaysong = (idSong) => {
+        // setIdSong(idSong)
+        handleSetSong(idSong)
+    }
+    useEffect(() => {
+        setIdSong(song.id)
+    }, [song])
+
+    return (
+        <>
+        <div className=" col-span-2 overflow-y-scroll">
+            <table className=" table-auto w-full">
+                <thead className="text-white h-12">
+                    <tr>
+                        <th className="w-[10%]">#</th>
+                        <th className="text-left">TITLE</th>
+                        <th className="w-[10%]">AUTHOR</th>
+                        <th className="w-[10%]"><i className="fa fa-download"></i></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {DataSongs.map((song, index) => (
+                    <tr key={index} 
+                        className={`body-background h-12 text-gray-900 hover:bg-slate-200 hover:text-blue-700 ${idSong === song.id && 'text-red-600 bg-slate-200'}`} 
+                        onClick={() => handlePlaysong(song.id)}
+                    >
+                        <td className="text-center">{index+1}</td>
+                        <td>{song.name}</td>
+                        <td className="text-center">{song.author}</td>
+                        <td className="text-center"><i href={song.url} className="fa fa-download"></i></td>
+                    </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+        </>      
+    )
+}
